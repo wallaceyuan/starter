c366ca12b989aa790731d3ed35b5bafa02c16c02
袁圆 <yuan.yuan486@chinaredstar.com>
2017-06-29 19:49:44 +0800

    first commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
-/* 入口启动文件 */
-import React from 'react'
-import { render } from 'react-dom'
+import {createStore} from 'redux';
+//import * as Redux from 'redux';
+//console.log(Redux)
 
-import App from './containers/index'
 
-if (__DEV__) {
-  console.info('[当前环境] 开发环境')
+//redux  applyMiddleware bindActionCreators combineReducers compose createStore
+
+//reducer 纯函数
+const counter = (count = 0,action) =>{
+  switch (action.type){
+    case 'INCREMENT':
+      return count + 1
+    case 'DECREMENT':
+      return count - 1
+    default:
+      return count
+  }
 }
-if (__PROD__) {
-  console.info('[当前环境] 生产环境')
+
+const initState = {
+  name:'guo',
+  city:'beijing'
 }
 
-const root = document.getElementById('app')
 
-render( <App />, root )
+//createStore 三个参数reducer initState enhancer
+let store = createStore(counter,initState)// store 包含 dispatch getState replaceReducer subscribe Symbol
+
+console.log(store.getState())
+
+
+//手动调用dispatch
+store.dispatch({type:'DECREMENT'})
